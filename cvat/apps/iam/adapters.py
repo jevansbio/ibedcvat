1434e5f0fe24b3374ee5e659c1be6058f59edb54
Julian Evans <jevansbio@gmail.com>
2023-07-19 12:02:22 +0200

    Automatically email superusers on registration, template tweaks

--- cvat/apps/iam/adapters.py
+++ cvat/apps/iam/adapters.py
@@ -5,8 +5,31 @@
 from django.http import HttpResponseRedirect
 from django.conf import settings
 
+from django.contrib.auth.models import User
+from django.contrib.sites.shortcuts import get_current_site
+
 from allauth.account.adapter import DefaultAccountAdapter
 
 class DefaultAccountAdapterEx(DefaultAccountAdapter):
+
+    def send_confirmation_mail(self, request, emailconfirmation, signup):
+        current_site = get_current_site(request)
+        activate_url = self.get_email_confirmation_url(request, emailconfirmation)
+        ctx = {
+            "user": emailconfirmation.email_address.user,
+            "activate_url": activate_url,
+            "current_site": current_site,
+            "key": emailconfirmation.key,
+        }
+        if signup:
+            email_template = "account/email/email_confirmation_signup"
+        else:
+            email_template = "account/email/email_confirmation"
+        self.send_mail(email_template, emailconfirmation.email_address.email, ctx)
+        superusers_emails = User.objects.filter(is_superuser=True).values_list('email')
+        for superuser_email in superusers_emails:
+            self.send_mail(email_template, superuser_email, ctx)            
+		
+
     def respond_email_verification_sent(self, request, user):
         return HttpResponseRedirect(settings.ACCOUNT_EMAIL_VERIFICATION_SENT_REDIRECT_URL)
